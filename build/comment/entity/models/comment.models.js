dd278e7d3aa1aa9f6a0402294e61497f2b4dc23b
joseniquen08 <j.armando0807@gmail.com>
2022-03-01 19:45:59 -0500

    deploy: files ready to deploy

--- build/comment/entity/models/comment.models.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CommentModel = void 0;
-const mongoose_1 = require("mongoose");
-const comment_schemas_1 = require("../schema/comment.schemas");
-exports.CommentModel = (0, mongoose_1.model)('Comment', comment_schemas_1.CommentSchema);
--- build/post/entity/models/post.models.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PostModel = void 0;
-const mongoose_1 = require("mongoose");
-const post_schemas_1 = require("../schemas/post.schemas");
-exports.PostModel = (0, mongoose_1.model)('Post', post_schemas_1.PostSchema);
